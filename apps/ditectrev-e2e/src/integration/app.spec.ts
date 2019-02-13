bb3432ed8424e4d254b485d87c02a60bddfdc367
Daniel Danielecki <daniel.danielecki@mail.com>
2019-02-13 12:00:44 +0100

    Change: name of default Cypress first test

--- apps/ditectrev-e2e/src/integration/app.spec.ts
+++ apps/ditectrev-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('Hello Nx', () => {
+describe('Hello Ditectrev', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
