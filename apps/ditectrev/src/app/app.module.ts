15ff03b15c31c6b1af47f6ffd9fa0e847c3c36b3
Daniel Danielecki <daniel.danielecki@mail.com>
2019-02-13 13:58:33 +0100

    Add: Angular Material

--- apps/ditectrev/src/app/app.module.ts
+++ apps/ditectrev/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
-
 import Agastya from 'agastya';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 new Agastya('ditectrev');
 
@@ -13,6 +14,7 @@ new Agastya('ditectrev');
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
