d5a35829a870110cc174c41e155a6862b0f24a76
Daniel Danielecki <daniel.danielecki@mail.com>
2019-02-28 14:28:38 +0100

    Add: TODO for unit test case

--- libs/home-components/src/lib/slider/slider.component.ts
+++ libs/home-components/src/lib/slider/slider.component.ts
@@ -11,6 +11,6 @@ declare let particlesJS: any;
 export class SliderComponent implements OnInit {
   ngOnInit(): void {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    particlesJS('particles-js', ParticlesConfig, function() {});
+    particlesJS('particles-js', ParticlesConfig, function() {}); // TODO: function() is not covered in the unit test, check it.
   }
 }
