f4fc39685d85f6e5cee74d7b697d48741ec8e123
Daniel Danielecki <daniel.danielecki@mail.com>
2019-03-05 15:44:43 +0100

    Add: about us components module

---
+++ libs/about-us-components/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'about-us-components',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/about-us-components'
+};
---
+++ libs/about-us-components/src/lib/about-us-components.module.spec.ts
@@ -0,0 +1,17 @@
+import {
+  async,
+  TestBed
+} from '@angular/core/testing';
+import { AboutUsComponentsModule } from './about-us-components.module';
+
+describe('AboutUsComponentsModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AboutUsComponentsModule]
+    }).compileComponents();
+  }));
+
+  it('should create about us components', () => {
+    expect(AboutUsComponentsModule).toBeDefined();
+  });
+});
