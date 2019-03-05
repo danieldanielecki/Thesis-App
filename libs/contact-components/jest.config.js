7cd8824fb18ad53894f1d87c5f6581a56877f8ca
Daniel Danielecki <daniel.danielecki@mail.com>
2019-03-05 16:46:43 +0100

    Add: contact components module

---
+++ libs/contact-components/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'contact-components',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/contact-components'
+};
---
+++ libs/contact-components/src/lib/contact-components.module.spec.ts
@@ -0,0 +1,17 @@
+import {
+  async,
+  TestBed
+} from '@angular/core/testing';
+import { ContactComponentsModule } from './contact-components.module';
+
+describe('ContactComponentsModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ContactComponentsModule]
+    }).compileComponents();
+  }));
+
+  it('should create contact components', () => {
+    expect(ContactComponentsModule).toBeDefined();
+  });
+});
