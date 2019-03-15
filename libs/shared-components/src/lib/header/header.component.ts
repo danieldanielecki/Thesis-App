512e64c910179cd960e04d009eaaa93a546d55f9
Daniel Danielecki <daniel.danielecki@mail.com>
2019-03-15 14:46:22 +0100

    Fix: menu responsiveness on medium devices

--- libs/shared-components/src/lib/header/header.component.ts
+++ libs/shared-components/src/lib/header/header.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +6,4 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
-  // TODO: Check it, something with RWD.
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
 }
