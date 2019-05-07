d07808ff57f3056df8fc7ca4362612f99f28765e
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-07 13:03:57 +0200

    Add: 1 TODO in contact

--- libs/contact/src/lib/contact/contact.component.ts
+++ libs/contact/src/lib/contact/contact.component.ts
@@ -138,5 +138,6 @@ export class ContactComponent {
     // TODO: Send en e-mail.
     console.log(form);
     // TODO: Reset the form.
+    // TODO: Add back-end validation, especially with accepted files format.
   }
 }
