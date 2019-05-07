8c2175761f137a58325f6fd4ad75bd2a0282f647
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-07 15:02:00 +0200

    Change: clean code contact module

--- libs/contact/src/lib/contact.module.ts
+++ libs/contact/src/lib/contact.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule,
   MatCardModule,
   MatCheckboxModule,
+  MatDatepickerModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatDatepickerModule,
+  MatListModule,
   MatNativeDateModule,
+  MatOptionModule,
   MatRadioModule,
   MatSelectModule,
-  MatOptionModule,
   ErrorStateMatcher,
   ShowOnDirtyErrorStateMatcher,
-  MatSlideToggleModule,
-  MatListModule
+  MatSlideToggleModule
 } from '@angular/material';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { Ng2TelInputModule } from 'ng2-tel-input';
 import {
   RecaptchaModule,
@@ -28,47 +28,47 @@ import {
   RecaptchaSettings
 } from 'ng-recaptcha';
 import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
     RouterModule.forChild([{ path: '', component: ContactComponent }]),
     CommonModule,
+    FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatDatepickerModule,
+    MatListModule,
     MatNativeDateModule,
+    MatOptionModule,
     MatRadioModule,
     MatSelectModule,
-    MatOptionModule,
     MatSlideToggleModule,
-    Ng2TelInputModule,
-    MatListModule,
-    RecaptchaModule,
-    RecaptchaFormsModule,
     MaterialFileInputModule,
-    FlexLayoutModule
+    Ng2TelInputModule,
+    RecaptchaModule,
+    RecaptchaFormsModule
   ],
   // TODO: Change the sample API key to a real one (as env variable).
   providers: [
     { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
     {
+      provide: RECAPTCHA_NONCE,
+      useValue: '<YOUR_NONCE_VALUE>' // TODO: Add nonce for CSP here (as env variable).
+    },
+    {
       provide: RECAPTCHA_SETTINGS,
       useValue: {
         siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
       } as RecaptchaSettings
-    },
-    {
-      provide: RECAPTCHA_NONCE,
-      useValue: '<YOUR_NONCE_VALUE>' // TODO: Add nonce for CSP here (as env variable).
     }
   ]
 })
