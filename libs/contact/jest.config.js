c013ecc9db4e3ca90174f410981ac7d5becc5c10
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-09 14:54:52 +0200

    Add: documentation for jest.config.js in contact

--- libs/contact/jest.config.js
+++ libs/contact/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   name: 'contact',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/contact',
-  transformIgnorePatterns: ['../../node_modules/(?!${ng2-tel-input})']
+  transformIgnorePatterns: ['../../node_modules/(?!${ng2-tel-input})'] // Required to avoid failing tests due to importing ES6 modules (here: ng2-tel-input module) from node_modules.
 };
