5323495517b9ebab03bd498006e0c7b16f772bf4
Daniel Danielecki <daniel.danielecki@mail.com>
2019-06-17 16:12:58 +0200

    Delete: environment variable for service worker

--- apps/ditectrev/src/app/app.module.ts
+++ apps/ditectrev/src/app/app.module.ts
@@ -3,7 +3,6 @@ declare const AGASTYA_API_KEY: string; // Declare Agastya API key secret.
 import Agastya from 'agastya';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@libs/core/src/index';
-import { environment } from '../environments/environment.prod';
 import { HomeModule } from '@libs/home/src/index';
 import { NgModule } from '@angular/core';
 import { NxModule } from '@nrwl/nx';
@@ -22,9 +21,7 @@ new Agastya(AGASTYA_API_KEY); // TODO: Check the documentation to make it proper
     NxModule.forRoot(),
     RoutingModule,
     SharedModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    }) // TODO: Check if shared modules shouldn't be loaded only in feature modules. Problem is how to fix footer/header to be available - put it to core?
+    ServiceWorkerModule.register('ngsw-worker.js') // TODO: Check if shared modules shouldn't be loaded only in feature modules. Problem is how to fix footer/header to be available - put it to core?
   ]
 })
 export class AppModule {}
