a3e39a35dc3943629b9c584df94f93343e79c6e2
Daniel Danielecki <daniel.danielecki@mail.com>
2019-06-17 15:54:08 +0200

    Add: service worker

--- apps/ditectrev/src/app/app.module.ts
+++ apps/ditectrev/src/app/app.module.ts
@@ -3,10 +3,12 @@ declare const AGASTYA_API_KEY: string; // Declare Agastya API key secret.
 import Agastya from 'agastya';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@libs/core/src/index';
+import { environment } from '../environments/environment';
 import { HomeModule } from '@libs/home/src/index';
 import { NgModule } from '@angular/core';
 import { NxModule } from '@nrwl/nx';
 import { RoutingModule } from './routing/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { SharedModule } from '@libs/shared/src/index';
 
 new Agastya(AGASTYA_API_KEY); // TODO: Check the documentation to make it proper.
@@ -19,7 +21,10 @@ new Agastya(AGASTYA_API_KEY); // TODO: Check the documentation to make it proper
     HomeModule,
     NxModule.forRoot(),
     RoutingModule,
-    SharedModule // TODO: Check if shared modules shouldn't be loaded only in feature modules. Problem is how to fix footer/header to be available - put it to core?
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }) // TODO: Check if shared modules shouldn't be loaded only in feature modules. Problem is how to fix footer/header to be available - put it to core?
   ]
 })
 export class AppModule {}
