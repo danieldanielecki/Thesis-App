cf1584027cc50f05f6373c0a29593e3f3eb0cfa3
Daniel Danielecki <daniel.danielecki@mail.com>
2019-08-02 08:17:50 +0200

    Fix: formatting issue

--- libs/contact/jest.config.js
+++ libs/contact/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   coverageDirectory: '../../coverage/libs/contact',
   name: 'contact',
-  preset: '../../jest.config.js'
+  preset: '../../jest.config.js',
   transformIgnorePatterns: ['../../node_modules/(?!${ng2-tel-input})'] // Required to avoid failing tests due to importing ES6 modules (here: ng2-tel-input module) from node_modules.
 };
